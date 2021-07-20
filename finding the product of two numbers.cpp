60c8cdcfeb77454c7aa21c91dd12ba4ed3f87c9c
Vijay <87227143+vijayethiraj@users.noreply.github.com>
2021-07-20 22:34:19 +0530

    Update finding the product of two numbers.cpp

--- finding the product of two numbers.cpp
+++ finding the product of two numbers.cpp
@@ -2,16 +2,10 @@
 using namespace std;
 
 int main() {
-  double num1, num2, product;
-  cout << "Enter two numbers: ";
-
-  // stores two floating point numbers in num1 and num2 respectively
-  cin >> num1 >> num2;
- 
-  // performs multiplication and stores the result in product variable
-  product = num1 * num2;  
-
-  cout << "Product = " << product;    
-    
-  return 0;
+    double x;
+    double y;
+    cout << "Enter two numbers: " << endl;
+    cin >> x;
+    cin >> y;
+    cout << "Product = " << (x * y) << endl;
 }
